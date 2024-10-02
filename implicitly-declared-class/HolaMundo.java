0a401a34b7a222d7dc949e4e453dd57d0d941c2a
brayanmnz <brayanmnz@hotmail.com>
2024-10-02 14:21:07 -0400

    jep-477 demo : implicitly declared classes examples

---
+++ implicitly-declared-class/HolaMundo.java
@@ -0,0 +1,9 @@
+String greetingMessage = "Hola, mundo!"; 
+
+String greetings() {
+	return greetingMessage;
+} 
+
+void main() {
+	System.out.println(greetings());
+}
---
+++ implicitly-declared-class/InteractWithConsole.java
@@ -0,0 +1,7 @@
+void main(){
+	println("Bienvenidos a otra sesión de JDK Fundamental Talks ");
+	
+	String name = readln("\n¿Cuál es tu nombre? - "); 
+	
+	println("\n \nBienvenido " + name);
+}
